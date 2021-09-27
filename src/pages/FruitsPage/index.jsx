9508d11d25a9942629814188a9ea28b6accd1b46
Laudemir <nlaudemir@gmail.com>
2021-09-27 15:35:01 -0300

    Segunda parte da atividade feita

--- src/pages/FruitsPage/index.jsx
+++ src/pages/FruitsPage/index.jsx
@@ -1,17 +1,24 @@
-//importando useSelector do react-redux
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFruit } from "../../store/modules/fruits/actions";
 
-const FruitsPage = () => {
-  //acessando o estado global e pegando o state fruits
+function FruitsPage() {
+  const [fruit, setFruit] = useState("");
+  const dispatch = useDispatch();
   const fruits = useSelector((state) => state.fruits);
+
+  const handleAddFruit = () => dispatch(addFruit(fruit));
 
   return (
     <div>
+      <input onChange={(e) => setFruit(e.target.value)} />
+      <button onClick={handleAddFruit}>Adicionar fruta</button>
+
       {fruits.map((fruit) => (
         <p key={fruit}> {fruit} </p>
       ))}
     </div>
   );
-};
+}
 
 export default FruitsPage;
--- src/store/modules/fruits/reducer.js
+++ src/store/modules/fruits/reducer.js
@@ -1,7 +1,11 @@
-const initialState = ["Banana", "Maçã", "Morango"];
+const initialState = ["Banana", "Maçã", "Kiwi"];
 
 const fruitsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD_FRUIT":
+      const { fruit } = action;
+      return [...state, fruit];
+
     default:
       return state;
   }
